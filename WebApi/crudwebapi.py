5b82c143b75b8cabe744d7e95cad857029b00bea
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-21 10:38:49 +0530

    we have connected our database our xampp server to our flask code before connection we have created databse name crudmethod in xampp server in whihc no table yet created

--- WebApi/crudwebapi.py
+++ WebApi/crudwebapi.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.secret_key = "secret_key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root: @127.0.0.1:3307/crudmethod'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
