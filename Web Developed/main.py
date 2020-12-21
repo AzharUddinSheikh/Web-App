fb9fb3cf1709cb08361dcadcf7230abdcc1d3650
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-21 19:42:54 +0530

    finally we have created post request to add contact info into our databse

--- Web Developed/main.py
+++ Web Developed/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -15,7 +15,7 @@ class Contacts(db.Model):
     name = db.Column(db.String(80), unique=False, nullable=False)
     phone_num = db.Column(db.String(12), unique=True, nullable=False)
     msg = db.Column(db.String(120), unique=True, nullable=False)
-    date = db.Column(db.String(12), unique=False, nullable=False)
+    date = db.Column(db.String(12), unique=False)
     email = db.Column(db.String(20), unique=False, nullable=False)
 
 
@@ -29,8 +29,20 @@ def about():
     return render_template('aboutblog.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+
+        '''add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contacts(name=name, phone_num=phone, msg=message, email=email)
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contactblog.html')
 
 
