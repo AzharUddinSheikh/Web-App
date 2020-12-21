024e08864bc1795a00fda334f828671b70a36aec
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2020-12-21 19:15:09 +0530

    we have created database in xampp server and also with the help of sql alchemy we connected out db also we define class name

--- Web Developed/main.py
+++ Web Developed/main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3307/codingthunder'
+
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    phone_num = db.Column(db.String(12), unique=True, nullable=False)
+    msg = db.Column(db.String(120), unique=True, nullable=False)
+    date = db.Column(db.String(12), unique=False, nullable=False)
+    email = db.Column(db.String(20), unique=False, nullable=False)
 
 
 @app.route('/')
